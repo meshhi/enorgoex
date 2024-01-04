52a6ed648ba6189cc1b441a9d1c58d2860b4ae12
Michael <yumythug@yandex.ru>
2024-01-04 16:28:35 +0300

    advantages

--- src/components/Content/Advantages/Card/Card.tsx
+++ src/components/Content/Advantages/Card/Card.tsx
@@ -5,18 +5,22 @@ import { Icon } from '@iconify/react/dist/iconify.js'
 const Card = ({text, icon}) => {
   return (
     <div className={s.card}>
-        <div className={s.card_icon}>
-            <div className={s.circle}></div>
-            <div className={s.icon_container}>
-                <Icon
-                icon={icon}
-                className={s.icon}
-                />
+        <div className={s.card_icon_container}>
+            <div className={s.card_icon}>
+                <div className={s.circle}></div>
+                <div className={s.icon_container}>
+                    <Icon
+                    icon={icon}
+                    className={s.icon}
+                    />
+                </div>
             </div>
         </div>
-        <header className={s.card_text}>
-            {text}
-        </header>
+        <div className={s.card_text_container}>
+            <header className={s.card_text}>
+                {text}
+            </header>
+        </div>
     </div>
   )
 }
