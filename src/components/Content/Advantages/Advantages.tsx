de84e59aa743098664857ebcddd306164a254969
Michael <yumythug@yandex.ru>
2024-01-04 16:02:49 +0300

    advantages

--- src/components/Content/Advantages/Advantages.tsx
+++ src/components/Content/Advantages/Advantages.tsx
@@ -5,16 +5,24 @@ import Card from './Card/Card'
 
 const advantageCards = [{
   "id": 1,
-  "icon": "card",
+  "icon": "iconamoon:like-bold",
   "text": "Опыт в сфере более 25 лет"
 }, {
   "id": 2,
-  "icon": "card",
+  "icon": "ph:phone",
   "text": "Всегда на связи"
 }, {
   "id": 3,
-  "icon": "card",
-  "text": "Согласовываем докуметнально"
+  "icon": "carbon:document",
+  "text": "Согласовываем документально"
+}, {
+  "id": 4,
+  "icon": "mingcute:lightning-line",
+  "text": "Соблюдаем правила и технологии"
+}, {
+  "id": 5,
+  "icon": "material-symbols-light:high-quality-outline-rounded",
+  "text": "Качественно и в срок"
 },]
 
 const Advantages = () => {
@@ -22,7 +30,7 @@ const Advantages = () => {
     <section className={s.advantages}>
         <CustomTitle title="Преимущества" position="center" color="black"></CustomTitle>
         <div className={s.advantages_list}>{
-          advantageCards.map(el => <Card key={el.id} text={el.text}></Card>)
+          advantageCards.map(el => <Card key={el.id} text={el.text} icon={el.icon}></Card>)
         }</div>
     </section>
   )
--- src/components/Content/Advantages/Card/Card.tsx
+++ src/components/Content/Advantages/Card/Card.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import s from './Card.module.scss'
+import { Icon } from '@iconify/react/dist/iconify.js'
 
-const Card = ({text}) => {
+const Card = ({text, icon}) => {
   return (
     <div className={s.card}>
-        {text}
+        <div className={s.card_icon}>
+            <div className={s.circle}></div>
+            <div className={s.icon_container}>
+                <Icon
+                icon={icon}
+                className={s.icon}
+                />
+            </div>
+        </div>
+        <header className={s.card_text}>
+            {text}
+        </header>
     </div>
   )
 }
