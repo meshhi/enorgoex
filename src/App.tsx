4d732a5cbb9106b807c8aca6fb159c96f7e692a0
Michael <yumythug@yandex.ru>
2024-01-07 22:22:01 +0300

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Layout from './components/Layout/Layout'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  } from 'react-router-dom';
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}></Route>
+        </Routes>
+      </Router>
   )
 }
 
